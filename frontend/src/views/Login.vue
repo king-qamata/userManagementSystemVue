0fe358a7b9b104afd221e0074113600bc7959ef2
limy <1154479896@qq.com>
2019-09-05 16:37:11 +0800

    UPDATE：优化登录界面，设置enter登录

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="login-container">
-    <h3>登录</h3>
-    <el-form ref="loginForm" :model="loginForm" :rules="fieldRules" status-icon label-width="100px" class="demo-ruleForm">
-      <el-form-item label="用户名" prop="account">
+    <h3>登 录</h3>
+    <el-form ref="loginForm" :model="loginForm" :rules="fieldRules" status-icon label-width="80px" class="demo-ruleForm">
+      <el-form-item label="账号" prop="account">
         <el-input v-model="loginForm.account" />
       </el-form-item>
-      <el-form-item label="密码" prop="password">
+      <el-form-item label="密码" prop="password" @keyup.enter.native="submitForm('loginForm')">
         <el-input v-model="loginForm.password" type="password" />
       </el-form-item>
 
-      <el-form-item>
-        <el-button type="primary" class="login-btn" @click="submitForm('loginForm')">提交</el-button>
+      <el-form-item class="login-btn">
+        <el-button type="primary" @click="submitForm('loginForm')">提 交</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -42,7 +42,7 @@ export default {
       this.$refs[formName].validate((valid) => {
         // 表单验证通过执行
         if (valid) {
-          console.log('表单验证通过')
+          // console.log('表单验证通过')
           const loginInfo = this.loginForm
           Login(loginInfo)
             .then((res) => {
@@ -58,7 +58,6 @@ export default {
               })
             })
         } else {
-          console.log('表单验证未通过')
           return false
         }
       })
@@ -69,12 +68,19 @@ export default {
 
 <style scoped>
 .login-container {
-  width: 500px;
+  margin: 180px auto;
+  width: 350px;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+  padding: 35px 35px 15px 0;
 }
 h3 {
+  margin-top: 0;
+  padding-left: 40px;
   text-align: center;
+  font-size: 30px;
 }
-login-btn {
-  margin: 0 auto;
+.login-btn {
+  margin-left: 80px;
 }
 </style>
